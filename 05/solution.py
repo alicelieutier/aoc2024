32aedd38f9abe7b0fa6b34b484f0cc3d8ece6fcf
Alice Lieutier <alice.lieutier@gmail.com>
2024-12-05 16:10:17 +0000

    small refactor... keeping the logic, changing it would make code cleaner but slightly less efficient or harder to read

--- 05/solution.py
+++ 05/solution.py
@@ -18,31 +18,45 @@ def data_from_file(filename):
     updates = (parse_update(line) for line in updates.split('\n') if line != '')
     return rules, updates
 
-def process_rules(rules):
+def middle_page_number(update):
+  return update[len(update) // 2]
+
+def is_ordered_function(rules):
   must_follow_map = {}
   for before, after in rules:
     if before not in must_follow_map:
       must_follow_map[before] = set()
     must_follow_map[before].add(after)
-  return must_follow_map
 
-def is_ordered(must_follow_map, update):
-  seen = set()
-  for page in update:
-    if must_follow_map.get(page, set()) & seen:
-      return False
-    seen.add(page)
-  return True
+  def is_ordered(update):
+    seen = set()
+    for page in update:
+      if must_follow_map.get(page, set()) & seen:
+        return False
+      seen.add(page)
+    return True
+  return is_ordered
 
-def middle_page_number(update):
-  return update[len(update) // 2]
+
+def sorted_function(rules):
+  def compare(a, b):
+    # nonlocal rules
+    if (a,b) in rules: return -1
+    if (b,a) in rules: return 1
+    print("Incomplete order!")
+
+  def custom_sorted(update):
+    return sorted(update, key=cmp_to_key(compare))
+  
+  return custom_sorted
+
 
 # The notation X|Y means that if both page number X and page
 # number Y are to be produced as part of an update,
 # page number X must be printed at some point before page number Y.
 def part_1(rules, updates):
-  must_follow_map = process_rules(rules)
-  return sum(middle_page_number(update) for update in updates if is_ordered(must_follow_map, update))
+  is_ordered = is_ordered_function(rules)
+  return sum(middle_page_number(update) for update in updates if is_ordered(update))
 
 def comparison_function(rules):
   def compare(a, b):
@@ -53,10 +67,10 @@ def comparison_function(rules):
   return compare
 
 def part_2(rules, updates):
+  custom_sorted = sorted_function(rules)
   total = 0
-  compare = comparison_function(rules)
   for update in updates:
-    new_update = sorted(update, key=cmp_to_key(compare))
+    new_update = custom_sorted(update)
     if update != new_update:
       total += middle_page_number(new_update)
   return total
