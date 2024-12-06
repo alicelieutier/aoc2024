b4e60446d72e4b41aa5ff03d10000664981f0977
Alice Lieutier <alice.lieutier@gmail.com>
2024-12-06 12:30:13 +0000

    refactor for style, but not sure if really better

--- 06/solution.py
+++ 06/solution.py
@@ -14,12 +14,6 @@ TEST_INPUT = '''
 '''
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
 
-# dx, dy
-UP = (0,-1)
-DOWN = (0,1)
-LEFT = (-1,0)
-RIGHT = (1,0)
-
 def parse_data(string):
   grid = [line.strip() for line in string.split('\n') if line != '']
   height = len(grid)
@@ -38,44 +32,43 @@ def inside_map(height, width, pos):
   x,y = pos
   return 0 <= x < width and 0 <= y < height
 
-# Guard always start going up
-def cycle_directions():
-  while True:
-    yield UP
-    yield RIGHT
-    yield DOWN
-    yield LEFT
+# dx, dy
+UP = (0,-1)
+DOWN = (0,1)
+LEFT = (-1,0)
+RIGHT = (1,0)
 
-def walk_guard(height, width, obstacles, guard_initial_position):
-  x, y = guard_initial_position
-  direction_gen = cycle_directions()
-  direction = next(direction_gen)
-  visited = set()
-  while inside_map(height, width, (x,y)):
-    visited.add((x,y))
-    dx, dy = direction
+def next_direction(prev_direction):
+  if prev_direction == UP: return RIGHT
+  if prev_direction == RIGHT: return DOWN
+  if prev_direction == DOWN: return LEFT
+  return UP
+
+def guard_walker(obstacles):
+  def next_state(guard_position, guard_direction):
+    x, y = guard_position
+    dx, dy = guard_direction
     if (x+dx, y+dy) in obstacles:
-      direction = next(direction_gen)
-      dx, dy = direction
-    else:
-      x,y = x+dx, y+dy
-  return len(visited)
+      return guard_position, next_direction(guard_direction)
+    return (x+dx, y+dy), guard_direction
+  return next_state
 
-def will_guard_loop(height, width, obstacles, guard_initial_position):
-  x, y = guard_initial_position
-  direction_gen = cycle_directions()
-  direction = next(direction_gen)
+def walk_guard(height, width, obstacles, guard_position, guard_direction=UP):
   visited = set()
-  while inside_map(height, width, (x,y)):
-    dx, dy = direction
-    if (x,y,dx,dy) in visited:
+  next_state = guard_walker(obstacles)
+  while inside_map(height, width, guard_position):
+    visited.add(guard_position)
+    guard_position, guard_direction = next_state(guard_position, guard_direction)
+  return visited
+
+def will_guard_loop(height, width, obstacles, guard_position, guard_direction=UP):
+  visited = set()
+  next_state = guard_walker(obstacles)
+  while inside_map(height, width, guard_position):
+    if (guard_position, guard_direction) in visited:
       return True
-    visited.add((x,y,dx,dy))
-    if (x+dx, y+dy) in obstacles:
-      direction = next(direction_gen)
-      dx, dy = direction
-    else:
-      x,y = x+dx, y+dy
+    visited.add((guard_position, guard_direction))
+    guard_position, guard_direction = next_state(guard_position, guard_direction)
   return False
 
 def string_from_file(filename):
@@ -84,17 +77,15 @@ def string_from_file(filename):
 
 def part_1(string):
   height, width, obstacles, guard_initial_position = parse_data(string)
-  return walk_guard(height, width, obstacles, guard_initial_position)
+  return len(walk_guard(height, width, obstacles, guard_initial_position))
 
 def part_2(string):
   height, width, obstacles, guard_initial_position = parse_data(string)
-  # lets brute force this !
+  possible_obstacle_locations = walk_guard(height, width, obstacles, guard_initial_position)
   counter = 0
-  for y in range(height):
-    for x in range(width):
-      if (x,y) == guard_initial_position: continue
-      if will_guard_loop(height, width, obstacles|{(x,y)}, guard_initial_position):
-        counter += 1
+  for x,y in possible_obstacle_locations - {guard_initial_position}:
+    if will_guard_loop(height, width, obstacles|{(x,y)}, guard_initial_position):
+      counter += 1
   return counter
 
 # Solution
