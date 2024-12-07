9ee3ef901082a4c43dc5fa5b6089b5147e155572
Alice Lieutier <alice.lieutier@gmail.com>
2024-12-07 19:35:34 +0000

    remove cache because it is not needed :( and forces to use tuples when the pattern matching create arrays

--- 07/solution.py
+++ 07/solution.py
@@ -1,5 +1,5 @@
 import os
-from functools import lru_cache
+from functools import cache
 
 TEST_INPUT = '''
 190: 10 19
@@ -26,16 +26,15 @@ def string_from_file(filename):
   with open(filename) as input:
     return input.read()
 
-@lru_cache
 def is_possible(value, numbers):
   match numbers:
-    case (): return False
-    case (last,): return value == last
-    case (*left, last):
+    case []: return False
+    case [last]: return value == last
+    case [*left, last]:
       if value % last != 0:
-        return is_possible(value - last, tuple(left))
-      return is_possible(value - last, tuple(left)) or is_possible(value // last, tuple(left))
-    
+        return is_possible(value - last, left)
+      return is_possible(value - last, left) or is_possible(value // last, left)
+
 def ends_with(value, number):
   return value > number and str(value).endswith(str(number))
 
@@ -44,19 +43,18 @@ def remove_suffix(value, suffix_number):
     return int(str(value)[:-len(str(suffix_number))])
   raise Exception('Tried to remove an invalid suffix')
 
-@lru_cache
 def is_possible_with_concat(value, numbers):
   match numbers:
-    case (): return False
-    case (last,): return value == last
-    case (*left, last):
+    case []: return False
+    case [last]: return value == last
+    case [*left, last]:
       branches = []
       if value % last == 0:
-        branches.append(is_possible_with_concat(value // last, tuple(left)))
+        branches.append(is_possible_with_concat(value // last, left))
       if ends_with(value, last):
-        branches.append(is_possible_with_concat(remove_suffix(value, last), tuple(left)))
+        branches.append(is_possible_with_concat(remove_suffix(value, last), left))
       if value > last:
-        branches.append(is_possible_with_concat(value - last, tuple(left)))
+        branches.append(is_possible_with_concat(value - last, left))
       return any(branches)
 
 def part_1(string):
@@ -68,8 +66,8 @@ def part_2(string):
   return sum(value for value, numbers in equations if is_possible_with_concat(value, numbers))
 
 # Solution
-print(part_1(string_from_file(INPUT_FILE))) # 
-print(part_2(string_from_file(INPUT_FILE))) # 
+print(part_1(string_from_file(INPUT_FILE)))
+print(part_2(string_from_file(INPUT_FILE)))
 
 # Tests
 assert is_possible(2, (1,1)) == True
@@ -84,9 +82,7 @@ assert is_possible_with_concat(15, (5,3)) == True
 assert is_possible_with_concat(3267, (81,40,27)) == True
 assert is_possible_with_concat(15666, (15,6,6,6)) == True
 assert is_possible_with_concat(7290, (6,8,6,15)) == True
-assert is_possible_with_concat(36, (1,2,3)) == True # 1 || 2 * 3 = 36
-assert is_possible_with_concat(103, (5,2,3)) == True # 5 * 2 || 3 = 103
-
+assert is_possible_with_concat(36, (1,2,3)) == True
+assert is_possible_with_concat(103, (5,2,3)) == True
 assert part_1(TEST_INPUT) == 3749
 assert part_2(TEST_INPUT) == 11387
-
