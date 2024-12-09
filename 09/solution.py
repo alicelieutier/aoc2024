1b748a509d09b90acfcce9e08981551e77a347e6
Alice Lieutier <alice.lieutier@gmail.com>
2024-12-09 11:04:09 +0000

    day 09 part 2

--- 09/solution.py
+++ 09/solution.py
@@ -1,43 +1,46 @@
 import os
-# from itertools import
 
 TEST_INPUT = '2333133121414131402'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
-
-def take_two(max):
-  a,b = 0,1
-  while b < max:
-    yield a,b
-    a, b = b+1, b+2
-    
-
-def parse_data(string):
-  file = True
-  file_id = 0
-  memory = []
-  for char in string:
-    if file:
-      for _ in range(int(char)):
-        memory.append(file_id)
-      file = False
-      file_id += 1
-    else:
-      for _ in range(int(char)):
-        memory.append(-1)
-      file = True
-  # debug_print(memory)
-  return memory
-
-def debug_print(memory):
-  print(''.join(str(n)[0] for n in memory))
 
 def string_from_file(filename):
   with open(filename) as input:
     return input.read()
 
+def debug_print(memory):
+  print(''.join(str(n)[0] for n in memory))
+
+def parse_data(string):
+  file = True
+  file_id = 0
+  memory_index = 0
+  memory = []
+  free_spaces = []
+  files = []
+  for char in string:
+    if file:
+      files.append((memory_index, int(char)))
+      for _ in range(int(char)):
+        memory.append(file_id)
+        memory_index += 1
+      file_id += 1
+      file = False
+    else:
+      free_spaces.append((memory_index, int(char)))
+      for _ in range(int(char)):
+        memory.append(-1)
+        memory_index += 1
+      file = True
+  return memory, free_spaces, files
+
+def debug_print(memory):
+  print(''.join(str(n)[0] for n in memory))
+
+def checksum(memory):
+  return sum(i*file_id for i, file_id in enumerate(memory) if file_id != -1)
+
 def part_1(string):
-  memory = parse_data(string)
-  checksum = 0
+  memory, _, _ = parse_data(string)
   left, right = 0, len(memory) - 1
   while left <= right:
     if memory[left] == -1:
@@ -45,18 +48,27 @@ def part_1(string):
         right -= 1
       memory[left], memory[right] = memory[right], memory[left]
       right -= 1
-    checksum += memory[left] * left
     left += 1
-  # debug_print(memory)
-  return checksum
+  return checksum(memory)
 
 def part_2(string):
-  pass
+  memory, free_spaces, files = parse_data(string)
+  for file_index, file_size in reversed(files):
+    for free_space_i, (space_index, space_size) in enumerate(free_spaces):
+      if space_index > file_index:
+        break
+      if space_size >= file_size:
+        for i, j in zip(range(space_index, space_index + file_size), range(file_index, file_index + file_size)):
+          memory[i], memory[j] = memory[j], memory[i]
+        free_spaces[free_space_i] = space_index + file_size, space_size - file_size
+        break
+  return checksum(memory)
+
 
 # Solution
-print(part_1(string_from_file(INPUT_FILE))) # 
-# print(part_2(string_from_file(INPUT_FILE))) # 
+print(part_1(string_from_file(INPUT_FILE))) # 6299243228569
+print(part_2(string_from_file(INPUT_FILE))) # 6326952672104
 
 # Tests
 assert part_1(TEST_INPUT) == 1928
-# assert part_2(TEST_INPUT) ==
+assert part_2(TEST_INPUT) == 2858
